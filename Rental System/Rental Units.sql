02f213391c4a4082463eea2dcbe593c5bc3525ae
srushtihj97 <105891223+srushtihj97@users.noreply.github.com>
2023-11-02 21:22:28 -0400

    Update Rental Units.sql

--- Rental System/Rental Units.sql
+++ Rental System/Rental Units.sql
@@ -5,7 +5,6 @@ USE `apartment_rent`;
 -- Host: localhost    Database: apartment_rent
 -- ------------------------------------------------------
 -- Server version	8.0.28
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -16,11 +15,9 @@ USE `apartment_rent`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `Amenities`
 --
-
 DROP TABLE IF EXISTS `Amenities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
